dd948c13e1aa705068c5f9625791e9a17636a484
GM921 <gmolla@mitre.org>
2026-01-26 13:20:35 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { JSX } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard';
+import Assessment from './pages/Assessment';
+import HistoryView from './pages/HistoryView';
+import Results from './pages/Results';
+import DomainResults from './pages/DomainResults';
+import AreaResults from './pages/AreaResults';
+import ImportExport from './pages/ImportExport';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+function App(): JSX.Element {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/assessment/:assessmentId" element={<Assessment />} />
+        <Route path="/history/:historyId" element={<HistoryView />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/results/:domainId" element={<DomainResults />} />
+        <Route path="/results/:domainId/:areaId" element={<AreaResults />} />
+        <Route path="/import-export" element={<ImportExport />} />
+        <Route path="/guide" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
