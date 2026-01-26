60d0ee9064fea5d2798afd737ae8f0b295af6cfd
GM921 <gmolla@mitre.org>
2026-01-26 13:15:41 -0500

    Delete test directory

--- test/vitest-axe.d.ts
+++
@@ -1,9 +0,0 @@
-import 'vitest';
-import type { AxeMatchers } from 'vitest-axe/matchers';
-
-declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unused-vars
-  interface Assertion<T = unknown> extends AxeMatchers {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends AxeMatchers {}
-}
